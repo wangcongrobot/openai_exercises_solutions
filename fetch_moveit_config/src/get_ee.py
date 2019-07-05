de8f53885c2ca92dc63540fa48fe77928ed11b64
aezquerro <albertoezquerro@gmail.com>
2019-07-05 14:52:21 +0000

    Added packages

---
+++ fetch_moveit_config/src/get_ee.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python
+
+import sys
+import copy
+import rospy
+import moveit_commander
+import moveit_msgs.msg
+import geometry_msgs.msg
+
+moveit_commander.roscpp_initialize(sys.argv)
+rospy.init_node('move_group_python_interface_tutorial', anonymous=True)
+
+robot = moveit_commander.RobotCommander()
+scene = moveit_commander.PlanningSceneInterface()    
+group = moveit_commander.MoveGroupCommander("arm")
+display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path', moveit_msgs.msg.DisplayTrajectory)
+
+#print "Current Pose:"
+#print group.get_current_pose()
+
+print "Current Joint Values:"
+print group.get_current_joint_values()
+
+moveit_commander.roscpp_shutdown()
